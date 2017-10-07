3ea8e6b11aac22595f900f5e8655d58913c045f3
unrelatedlabs <peter@pkuhar.com>
2017-10-07 01:11:04 -0700

    updated for use on web with webpack
    Not sure how to hadle this so it can also be used localy

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = {
 	helpers: require('./dist/bluetooth.helpers'),
 	get classic() {
 		var bluetooth = require('./dist/api.classic');
-		require('./dist/adapter.noble')(bluetooth);
+		require('./dist/adapter.evothings')(bluetooth);
 		return bluetooth;
 	},
 	get webbluetooth() {
 		var bluetooth = require('./dist/api.web-bluetooth');
-		require('./dist/adapter.noble')(bluetooth);
+		require('./dist/adapter.evothings')(bluetooth);
 		return bluetooth;
 	}
 };
